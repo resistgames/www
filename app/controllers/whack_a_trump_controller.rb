83d56a21da80afb090e569204098e7570eba557d
wlaurance <w.laurance@gmail.com>
2017-03-25 20:45:50 -0700

    Change titles + ga analytics

--- app/controllers/whack_a_trump_controller.rb
+++ app/controllers/whack_a_trump_controller.rb
@@ -4,5 +4,6 @@ class WhackATrumpController < ApplicationController
     @og_title = "Whack Donald Trump on the head"
     @og_description = "How many times can you hit Donald Trump? Try your luck in whack-a-trump"
     @og_image = view_context.image_url "whack_a_trump/ogshare.png"
+    @title = "Whack-A-Trump"
   end
 end
