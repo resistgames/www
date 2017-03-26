8e8963a7d75ffea08517a83edd5d7f70c57b7cec
wlaurance <w.laurance@gmail.com>
2017-03-26 15:16:38 -0700

    copy

--- app/controllers/whack_a_trump_controller.rb
+++ app/controllers/whack_a_trump_controller.rb
@@ -2,7 +2,7 @@ class WhackATrumpController < ApplicationController
   def index
     @og_url = request.original_url
     @og_title = "Whack Donald Trump on the head"
-    @og_description = "How many times can you hit Donald Trump? Try your luck in whack-a-trump"
+    @og_description = "How many times can you hit Donald Trump? Try your luck in Whack-A-Trump. A Wac-A-Mole like game."
     @og_image = view_context.image_url "whack_a_trump/ogshare.png"
     @title = "Whack-A-Trump"
   end
