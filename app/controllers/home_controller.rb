ae81fcd18d0fbb55b28a2beae7f62a9f1e9c2df5
wlaurance <w.laurance@gmail.com>
2017-03-26 20:47:13 -0700

    marketing pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,13 @@ class HomeController < ApplicationController
   def about
     @og_url = request.original_url
     @og_title = "Resist and Code"
+    @title = "About"
     @og_description = "Join the resistance and build political games"
+  end
+
+  def join
+    @og_url = request.original_url
+    @title = "Join the Resistance"
+    @og_description = "Fight Trump. Join the Resistance. Build, play, and support Resist Games."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/about' => 'home#about'
+  get '/join' => 'home#join'
+  get '/thankyou' => 'home#thankyou'
   # Get Whack a Trump
   get 'whack-a-trump' => 'whack_a_trump#index'
 end
