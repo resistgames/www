56a4dda9c2d7be65144377105ab833b10a9b97fe
wlaurance <w.laurance@gmail.com>
2017-03-27 20:53:37 -0700

    make the about content the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,4 @@
 class HomeController < ApplicationController
-  def index
-    @og_url = request.original_url
-    @og_title = "Resist and Code"
-    @og_description = "Join the resistance and build political games"
-  end
-
   def about
     @og_url = request.original_url
     @og_title = "Resist and Code"
