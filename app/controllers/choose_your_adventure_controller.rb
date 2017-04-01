74912082704c56e3b3aa517f4ac1fac80ff912b5
wlaurance <w.laurance@gmail.com>
2017-04-01 09:53:27 -0700

    resize canvas

--- app/controllers/choose_your_adventure_controller.rb
+++ app/controllers/choose_your_adventure_controller.rb
@@ -1,5 +1,9 @@
 class ChooseYourAdventureController < ApplicationController
   def index
     @slug = params[:slug]
+    @no_container = true
+    @page_specific_javascript = [
+      'choose_your_adventure'
+    ]
   end
 end
